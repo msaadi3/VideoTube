d3b7cdbbc039ffb469dd83cc0ddef06849f6ad25
msaadi3 <113223551+msaadi3@users.noreply.github.com>
2024-10-15 19:24:49 +0500

    just started this project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import NavAndSideBar from './mycomponents/NavAndSideBar.jsx';
+function App() {
+  return (
+    <>
+      <NavAndSideBar />
+    </>
+  );
+}
+
+export default App;
