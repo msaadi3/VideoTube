c80fe7d985edfe0e988ca9489628f61481649859
msaadi3 <113223551+msaadi3@users.noreply.github.com>
2024-10-16 10:45:49 +0500

    routing, fixed nav and side bar, ChannelPage needs to be fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import NavAndSideBar from './mycomponents/NavAndSideBar.jsx';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AppLayout from './mycomponents/AppLayout.jsx';
+import HomePage from './mycomponents/HomePage.jsx';
+import VideoPlayerPage from './mycomponents/VideoPlayerPage.jsx';
+import ChannelPage from './mycomponents/ChannelPage.jsx';
+import UpdateChannelInfo from './mycomponents/UpdateChannelInfo.jsx';
 function App() {
   return (
-    <>
-      <NavAndSideBar />
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<AppLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path='video/:id' element={<VideoPlayerPage />} />
+          <Route path='channel/:id' element={<ChannelPage />} />
+          <Route path='channel/update' element={<UpdateChannelInfo />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
